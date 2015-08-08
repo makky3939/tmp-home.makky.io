5bf08aa385ce274aa9e914abe1379aeac923110d
Masaki KOBAYASHI <makky.4d6b.3f5@gmail.com>
2015-08-08 22:53:14 +0900

    set columns

--- db/migrate/20150727081102_create_temperatures.rb
+++ db/migrate/20150727081102_create_temperatures.rb
@@ -1,8 +1,12 @@
 class CreateTemperatures < ActiveRecord::Migration
   def change
     create_table :temperatures do |t|
+      t.references :log, null: false
+      t.float :value, null: false
 
       t.timestamps null: false
     end
+    
+    add_index :temperatures, :log_id
   end
 end
