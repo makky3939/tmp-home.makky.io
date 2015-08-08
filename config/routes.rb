f3c3ae42c4713b264fd8e0c3321cca48c41fd860
Masaki KOBAYASHI <makky.4d6b.3f5@gmail.com>
2015-08-08 22:53:39 +0900

    fix route setting

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
-      resources :log, only: [:create]
+      resources :logs, only: [:create]
     end
   end
 
