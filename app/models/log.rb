1a247fc029484791670eeee6b8fee411e889ab94
Masaki KOBAYASHI <makky.4d6b.3f5@gmail.com>
2015-08-14 23:39:17 +0900

    markup dashboard

--- app/models/log.rb
+++ app/models/log.rb
@@ -3,4 +3,6 @@ class Log < ActiveRecord::Base
   has_one :humidity
   has_one :temperature
   belongs_to :location
+  
+  default_scope { includes(:temperature, :humidity, :atmosphere, :location) }
 end
